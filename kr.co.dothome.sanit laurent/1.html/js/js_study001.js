4c31ccb4d42ba711040abf3718b1879b84e113a1
lee gun <dlrjs1509@naver.com>
2022-07-05 21:14:26 +0900

    0705

---
+++ kr.co.dothome.sanit laurent/1.html/js/js_study001.js
@@ -0,0 +1,37 @@
+// Boolean study
+
+// 불리언(boolean)은 논리학에서 참(true)과 거짓(false)을 나타내는 데 사용됩니다.
+// 또한, 논리 연산(logical operation)이란 주어진 논리식을 판단하여 참(true)과 거짓(false)을 결정하는 연산입니다.
+// const amIfat = null;
+// let something;
+// console.log(something);
+// console.log(amIfat);
+
+// true = 1 참 켜지다, false = 0 거짓 꺼지다 
+
+// null = 아무것도 없음이라는 값 (즉 const amIfat = null; 는 amIfat 이라는 변수명에
+// 아무것도 없다라는 값을 가진 null을 대입한 것)
+// let something; 이런식으로 변수를 선언하면 underfined 값이 나옴 underfined는 저런식으로 변수에
+// 아무런 값을 넣지 않았을 때 나옴 null은 없다라는 값을 넣어준 거고 undefined는 값을 넣지 않은 것
+
+
+// Array study
+
+// const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat" , "sun"];
+// console.log(daysOfWeek);
+// for(let i = 0; i >= daysOfWeek.length; i++){
+//     console.log("지금 현재" + daysOfWeek[0] + "요일을 지나가고 있고 다음 남은 요일은"+ daysOfWeek[i] + "요일이다");
+// };
+
+// console.log(daysOfWeek);
+
+
+
+// Object study
+// const palyer = {
+//     name: "lee",
+//     points: 10000,
+//     fat: false
+// }
+// console.log(palyer.name);
+// console.log(palyer["name"]);
