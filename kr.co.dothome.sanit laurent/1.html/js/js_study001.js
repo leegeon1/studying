875bf44a7284b6ba6a8f825660cf4af1860b95ba
lee gun <dlrjs1509@naver.com>
2022-07-08 13:38:42 +0900

    0708

--- kr.co.dothome.sanit laurent/1.html/js/js_study001.js
+++ kr.co.dothome.sanit laurent/1.html/js/js_study001.js
@@ -35,3 +35,224 @@
 // }
 // console.log(palyer.name);
 // console.log(palyer["name"]);
+
+// day 2022 07 06
+
+// Functions
+ 
+// function sayHello(nameOfPerson , age){
+//     console.log("hello my name is "+nameOfPerson+" and i'm " + age);
+// };
+// sayHello("hi" , 21)
+// sayHello("bye" , 15)
+// sayHello("nice" , 22)
+
+// function plus(a,b){
+//     console.log(a + b);
+// }
+// function divide(a,b){
+//     console.log(a / b);
+// }
+// plus(60, 8);
+// divide(98, 20);
+
+// const player = {
+//     name: "geon",
+//     sayHello: function(outerPersonName){
+    // 보다시피 위에 function하고 지금 오브젝트에 펑션이랑 구문이 다름
+//     console.log("hello " + outerPersonName);
+//     },
+// };
+// console.log(player.name);
+// player.sayHello("juhawn");
+// player.sayHello("Geon");
+
+// const calculator = {
+//     add: function(a,b){
+//         alert(a + b);
+//     },
+//     min: function(a,b){
+//         alert(a - b);
+//     },
+//     div: function(a,b){
+//         alert(a / b);
+//     },
+//     power: function(a,b){
+//         alert(a ** b);
+//     },
+// };
+// calculator.add(1,5);
+// calculator.min(4,2);
+// calculator.div(15,3);
+// calculator.power(4,5);
+// console.log(calculator.add(2,3));
+
+// const age = 96;
+// function calculateKrAge(ageOfForeinger){
+//     return ageOfForeinger + 2;
+// };
+
+// const KrAge = calculateKrAge(age);
+// console.log(KrAge);
+
+// const calculator = {
+//     add: function(a,b){
+//         return a + b;
+//     },
+//     min: function(a,b){
+//         return a - b;
+//     },
+//     div: function(a,b){
+//         return a / b;
+//     },
+//     power: function(a,b){
+//         return a ** b;
+//     },
+// };
+// const addResult = calculator.add(2,3);
+// console.log(addResult);
+
+
+// const calculator = {
+//     add: function(a,b){
+//         return a + b;
+//     },
+//     min: function(a,b){
+//         return a - b;
+//     },
+//     div: function(a,b){
+//         return a / b;
+//     },
+//     power: function(a,b){
+//         return a ** b;
+//     },
+// };
+// const addResult = calculator.add(4,3);
+// const minResult = calculator.min(addResult, 5);
+// const divResult = calculator.div(10,minResult);
+// const powerResult = calculator.power(addResult,divResult);
+
+
+
+
+// const age = parseInt(prompt("how old are you?"));
+
+// console.log(typeof age ,typeof parseInt(age));
+
+
+// Conditionals part Two 조건문
+
+// const age = parseInt(prompt("how old are you?"));
+// console.log(isNaN(age));
+
+// if(isNaN(age)){
+//     console.log("please write a number");
+// }else[
+//     console.log("thank you for writing your age")
+// ]
+
+// isNaN = 변수 or 스트링,넘버를 숫자인지 아닌지 판별해줌
+
+// const age = parseInt(prompt("how old are you?"));
+
+// if(isNaN(age) || age < 0){
+//     console.log("please write a real positive number");
+// }else if(age < 18){
+//     console.log("yor are too young");
+// }else if(age >= 18 && age <= 50){
+//     console.log("you can drink");
+// }else if(age > 50 && age <= 80){
+//     console.log("you should exercise");
+// }else if(age > 80){
+//     console.log("you can do whatever want");
+// }else{
+
+// }
+
+// true || true === true
+// false || true === true
+// true || false === true
+// false || false === false
+
+// true && true === true
+// false && true === false
+// true && false === false
+// false && false === false
+
+
+
+// const age = parseInt(prompt("how old are you?"));
+
+// if(isNaN(age) || age < 0){
+//     console.log("please write a real positive number");
+// }else if(age < 18){
+//     console.log("yor are too young");
+// }else if(age >= 18 && age <= 50){
+//     console.log("you can drink");
+// }else if(age > 50 && age <= 80){
+//     console.log("you should exercise");
+// }else if(age === 100){
+//     console.log("wow you are wise");
+// }else if(age > 80){
+//     console.log("you can do whatever want");
+// }
+
+// if((a && b) || (c && d)){
+
+// }
+
+
+// 2022-07-07
+
+// document.title = 'hello ! from JS!' 
+// const title =  document.getElementById("title");
+// title.innerText = "got you!"
+
+// console.log(title.id);
+// console.log(title.className);
+
+// const hellos = document.getElementsByClassName("hello");
+// const title = document.getElementsByTagName("h1");
+
+// document.querySelector는 css의 선택자를 사용해서 class hello를
+// 찾음
+
+// const title = document.querySelector(".hello h1");
+
+// title.innerText = "hello";
+// title.style.color = "blue";
+
+// function handleTitleClick(){
+//     title.style.color = "red";
+//     title.innerText = "that was a right click!"
+// };
+
+// function handleMouseEnter(){
+//     title.innerText = "Mouse is here!"
+//     title.style.color = "green";
+// }
+// function handleMouseLeave(){
+//     title.innerText = "Mouse is gone!"
+//     title.style.color = "blue";
+// }
+// function handleMouseResize(){
+//     title.innerText = "you just resized!"
+//     title.style.color = ""
+// }
+// function handleWindowCopy(){
+//     alert("copier!");
+// }
+// function handleWindowOffline(){
+//     alert("SOS no Wife");
+// }
+// function handleWindowOnline(){
+//     alert("All good");
+// }
+// title.addEventListener("click",handleTitleClick);
+// title.onclick = handleTitleClick;
+// title.addEventListener("mouseenter",handleMouseEnter);
+// title.addEventListener("mouseleave",handleMouseLeave);
+// window.addEventListener("resize",handleMouseResize);
+// window.addEventListener("copy",handleWindowCopy);
+// window.addEventListener("offline",handleWindowOffline);
+// window.addEventListener("online",handleWindowOnline);
